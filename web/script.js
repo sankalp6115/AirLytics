b39cfdb7b2226e25a98d863152ed8becfdf7e911
sankalp6115 <sankalpomar6115@gmail.com>
2025-08-30 20:48:30 +0530

    Added more essential features

--- web/script.js
+++ web/script.js
@@ -3,8 +3,8 @@
       const data = await res.json();
 
       document.getElementById("air-quality").innerHTML = `
+      🌍 City: ${data.city}<br>
         <div class="aqi-indicator"></div>
-        🌍 City: ${data.city}<br>
         🌫️ AQI: ${data.aqi}<br>
         🌡️ Temperature: ${data.temperature}°C<br>
         ⛅ Condition: ${data.condition}<br>
@@ -31,3 +31,27 @@
 
     // section.pollutants
     //for each poll, [safe, moderate, dangerous]
+
+
+    const data = {
+        aqi: 100,
+        temp: 35,
+        condition: "Rainy",
+        Humidity: 20,
+        pm25: 20,
+        pm10: 200,
+        no2: 300,
+        o3: 40,
+        tds: 100,
+        ph: 6.8 
+    }
+
+    const ranges = {
+        aqi: [100, 250, 400],
+        pm25: [60, 90, 150],
+        pm10: [100, 300, 400],
+        no2: [80, 180, 400],
+        o3: [100, 200, 600],
+        tds: [150, 300, 500],
+        ph: []
+    }

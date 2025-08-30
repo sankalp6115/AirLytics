530e8ef6b7d8ce03080e01b73224614b772e5eee
sankalp6115 <sankalpomar6115@gmail.com>
2025-08-30 18:28:55 +0530

    Initial Commit

--- web/script.js
+++ web/script.js
@@ -1,14 +1,14 @@
-// Weather Data
     async function getWeather(city) {
       const res = await fetch(`http://localhost:3000/api/weather?city=${city}`);
       const data = await res.json();
 
-      document.getElementById("output").innerHTML = `
+      document.getElementById("air-quality").innerHTML = `
         🌍 City: ${data.city}<br>
-        🌡️ Temperature: ${data.temperature}°C<br>
-        💧 Humidity: ${data.humidity}%<br>
-        ⛅ Condition: ${data.condition}<br>
+        <div class="aqi-indicator'></div>
         🌫️ AQI: ${data.aqi}<br>
+        🌡️ Temperature: ${data.temperature}°C<br>
+        ⛅ Condition: ${data.condition}<br>
+        💧 Humidity: ${data.humidity}%<br>
       `;
     }
 

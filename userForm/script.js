f9c9f14dda8bc600880f2d271fc1cbb28ab9d0b2
sankalp6115 <sankalpomar6115@gmail.com>
2025-08-31 06:35:41 +0530

    Implementation of fastAPI server

--- userForm/script.js
+++ userForm/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "http://127.0.0.1:3000";
 // Form handling for userForm
 const age = document.querySelector("#age");
 const pincode = document.querySelector("#pincode");
